2b534b067ba0381c8420424568f0c64cd4529165
Dxh7788 <xiaohong.dong@htouhui.com>
2018-01-04 15:06:23 +0800

    first TDD example

---
+++ src/test/java/com/xpdev/day01/TestTemplate.java
@@ -0,0 +1,15 @@
+package com.xpdev.day01;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author xh.d
+ * @date 2018/1/4 14:16
+ */
+public class TestTemplate {
+    @Test
+    public void oneVariable(){
+        assertEquals("aaa","bbb");
+    }
+}
