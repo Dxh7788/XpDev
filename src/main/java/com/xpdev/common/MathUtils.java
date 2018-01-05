230abd1f21621640b9be04d74fedf3b882194039
Dxh7788 <anan1215>
2018-01-05 11:13:53 +0800

    we

--- src/main/java/com/xpdev/common/MathUtils.java
+++ src/main/java/com/xpdev/common/MathUtils.java
@@ -8,7 +8,6 @@ public class MathUtils {
     public static int add(int a, int b) {
         
         System.out.println("两数相乘....");
-        System.out.println("两数相除....");
         System.out.println("专治各种不服....");
         return a + b;
     }
